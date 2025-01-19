2387472ed6edf719620b15b27782aa764e22e35e
Corwin Wright <git.hub@corwin.me.uk>
2025-01-19 15:35:24 +0000

    minor modifications to wg2013 code

--- GWAnalysis/make_wg2013_noisefloor.m
+++ GWAnalysis/make_wg2013_noisefloor.m
@@ -15,19 +15,22 @@ clear all
 
 %instrument selection
 Settings.Instrument = 'HIRDLS';
-Settings.OutFile = ['noisefloor_',Settings.Instrument,'.mat'];
+Settings.OutFile = [LocalDataDir,'/corwin/noisefloor_',Settings.Instrument,'.mat'];
+
+%processing choices
+Settings.HeightScaleIn = 15:1:80;
 
 %statistical choices
 Settings.Percentile    = 99;    %what percentile of the distribution is the stored cutoff?
-Settings.NProfilePairs = 10000;  %how many profile-pairs should we use in each box?
+Settings.NProfilePairs = 2500;  %how many profile-pairs should we use in each box?
 
-%output interpolation choices
+%output grid to interpolate to
 Settings.LzGrid   = logspace(log10(60),log10(1),40);  %vertical wavelength grid
 Settings.AltScale = 15:5:80;                          %height scale
 
 %time handling
-Settings.DayRange   = [1,365]; %calendar days to range over
-Settings.WindowSize = 15;    %calendar days in window
+Settings.DayRange   = [1,30];% [1,365]; %calendar days to range over
+Settings.WindowSize = 14;   %calendar days in window
 Settings.DayBinStep = 7;    %days stepped between each window
 Settings.Years      = 2000:1:2020;  %years of data to use
 
@@ -37,8 +40,8 @@ Settings.LatBinStep = 5;
 Settings.LatBinSize = 5;
 
 Settings.LonRange   = [-180,180];
-Settings.LonBinStep = 20;
-Settings.LonBinSize = 40;
+Settings.LonBinStep = 30;
+Settings.LonBinSize = 30;
 
 %progress outputting
 Settings.Verbose    = 1;
@@ -72,17 +75,19 @@ for iDay=1:1:numel(Results.DayScale);
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
   clear Store
-  if Settings.Verbose; textprogressbar(['Loading data for ',num2str(Settings.WindowSize),' day range centred on calendar day ', num2str(Results.DayScale(iDay)),':  ']); end
+  if Settings.Verbose; textprogressbar(['Loading ',Settings.Instrument,' data for ',num2str(Settings.WindowSize),' days around d', num2str(Results.DayScale(iDay)),':  ']); end  
   for iYear=1:1:numel(Settings.Years)
 
     %get data
     DaysInWindow = 0.5.*[-1,1].*Settings.WindowSize + Results.DayScale(iDay);
-    ThisChunk = get_limbsounders(datenum(Settings.Years(iYear),1,[DaysInWindow]),Settings.Instrument, ...
-                                 'DateWarning',false,'GetHindleyPWs',true);
+    ThisChunk = get_limbsounders(datenum(Settings.Years(iYear),1,DaysInWindow),Settings.Instrument, ...
+                                 'DateWarning',false,'GetHindleyPWs',true, ...
+                                 'HeightScale',Settings.HeightScaleIn, ...
+                                 'TimeHandling',1);
 
     %append to the stored data
     if numel(ThisChunk.Lat) > 0; 
-      if ~exist('Store'); Store = ThisChunk; else;Store = cat_struct(Store,ThisChunk,1); end
+      if ~exist('Store','var'); Store = ThisChunk; else;Store = cat_struct(Store,ThisChunk,1); end
     end
 
     if Settings.Verbose; textprogressbar(iYear./numel(Settings.Years).*100); end
@@ -90,62 +95,38 @@ for iDay=1:1:numel(Results.DayScale);
   if Settings.Verbose; textprogressbar('!'); end
 
   %check we have any data
-  if ~exist('Store');  continue; end
-
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %% identify the points present in each region
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  %create an array to store point indices
-  RegionIndices = NaN(numel(Results.LatScale) .* numel(Results.LonScale),Settings.NProfilePairs);
-
-  %assign a nominal lat and lon to each profile
-  NomLon = nanmedian(Store.Lon,2);
-  NomLat = nanmedian(Store.Lat,2);
-
-  %fill the indices
-  iBox = 0;
-  for iLat=1:1:numel(Results.LatScale)
-    for iLon=1:1:numel(Results.LonScale)
-
-        %get bin bounds
-        iBox = iBox+1;
-        x = Results.LonScale(iLon)+0.5.*Settings.LonBinStep + [-1,1].*0.5.*(Settings.LonBinSize);
-        y = Results.LatScale(iLat)+0.5.*Settings.LatBinStep + [-1,1].*0.5.*(Settings.LatBinSize);
-
-        %find data in bin
-        if min(x) < -180      
-          xi = [find(NomLon > min(x)+360);find(NomLon < max(x))];
-        elseif max(x) > 180;
-          xi = [find(NomLon > min(x));find(NomLon < max(x) - 360)];
-        else
-          xi = inrange(NomLon,x);
-        end
-        idx = intersect(xi,inrange(NomLat,y));
-        if numel(idx) > 0;
-
-          %choose a random order of profiles, with repeat sampling permitted
-          RegionIndices(iBox,:) = idx(randi(numel(idx),[Settings.NProfilePairs,1]));
-        end
-        
-
-    end; clear iLon idx x y xi
-  end; clear iLat iBox NomLat NomLon
-
-  %reshape to give access to lons and lats in a logical way
-  RegionIndices = reshape(RegionIndices,[numel(Results.LonScale),numel(Results.LatScale),Settings.NProfilePairs]);
+  if ~exist('Store','var');  continue; end
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %% now, let's ST all the profiles using Alex08, but retaining the full ST
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+  %assign a nominal lat and lon to each profile
+  NomLon = median(Store.Lon,2,'omitnan');
+  NomLat = median(Store.Lat,2,'omitnan');
+
   if Settings.Verbose; textprogressbar('Computing noise floor '); k= 0;end
-  for iLat=1:1:numel(Results.LatScale)
-    for iLon=1:1:numel(Results.LonScale)
+  for iLat=1:1:numel(Results.LatScale)-1
+    for iLon=1:1:numel(Results.LonScale)-1
+
+      %get bin bounds
+      x = Results.LonScale(iLon)+0.5.*Settings.LonBinStep + [-1,1].*0.5.*(Settings.LonBinSize);
+      y = Results.LatScale(iLat)+0.5.*Settings.LatBinStep + [-1,1].*0.5.*(Settings.LatBinSize);
+
+      %find profiles in bin
+      if min(x) < -180
+        xi = [find(NomLon > min(x)+360);find(NomLon < max(x))];
+      elseif max(x) > 180;
+        xi = [find(NomLon > min(x));find(NomLon < max(x) - 360)];
+      else
+        xi = inrange(NomLon,x);
+      end
+      idx = intersect(xi,inrange(NomLat,y));
+      if numel(idx) == 0; continue; end
+      RegionIndices = idx(randi(numel(idx),[Settings.NProfilePairs,1]));
 
       %get randomly-selected profiles
-      if nansum(RegionIndices(iLon,iLat,:),"all") == 0; continue; end
-      Data = reduce_struct(Store,RegionIndices(iLon,iLat,:),{},1);
+      Data = reduce_struct(Store,RegionIndices,{},1);
 
       %st them
       GWs = gwanalyse_limb(Data,'Analysis',2,'Filter','Hindley23','FullST',true,'Verbose',false);
@@ -163,17 +144,19 @@ for iDay=1:1:numel(Results.DayScale);
       %... in Lz...
       CutOff = interp_1d_ndims(1./GWs.Freqs,CutOff,Settings.LzGrid,2);
       %... and in height...
-      CutOff = interp_1d_ndims(nanmean(GWs.Alt,1),CutOff,Results.AltScale,1);      
+      z = Settings.HeightScaleIn; good = ~isnan(z); z = z(good); CutOff = CutOff(good,:); 
+      if numel(good) < 2; continue; end
+      CutOff = interp_1d_ndims(z,CutOff,Results.AltScale,1);      
 
       %and store
       Results.CutOff(iDay,iLon,iLat,:,:) = CutOff;
 
-      clear a b CutOff GWs Data CovAmp
+      clear a b CutOff GWs Data CovAmp z good RegionIndices
 
-      if Settings.Verbose; k = k+1;textprogressbar(k./(numel(Results.LatScale) .* numel(Results.LonScale)).*100); end
+      if Settings.Verbose; k = k+1;textprogressbar(k./((numel(Results.LatScale)-1) .* (numel(Results.LonScale)-1)).*100); end
     end
-  end; clear k iLat iLon RegionIndices Store
-  if Settings.Verbose; textprogressbar('!'); end  
+  end; clear k iLat iLon Store NomLon NomLat
+  if Settings.Verbose; textprogressbar(100); textprogressbar('!'); end  
 
 
 
@@ -189,7 +172,10 @@ Results.LatScale = Results.LatScale+Settings.LatBinStep./2;
 Results.LonScale = Results.LonScale+Settings.LonBinStep./2;
 Results.DayScale = Results.DayScale+Settings.DayBinStep./2;
 
-Results.CutOff = Results.CutOff(1:end-1,1:end-1,1:end-1,:,:);
+%remove bins that lie off the end of the world
+Results.CutOff   = Results.CutOff(:,1:end-1,1:end-1,:,:);
+Results.LonScale = Results.LonScale(1:end-1);
+Results.LatScale = Results.LatScale(1:end-1);
 
 
 save(Settings.OutFile,'-struct','Results','-v7.3')
